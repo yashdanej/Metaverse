6130798415e87e4753758ee56fdd2595e3e6b6c2
Yash Danej <103168022+yashdanej@users.noreply.github.com>
2023-10-06 08:37:49 +0530

    Delete _Backend directory

--- _Backend/router/messageRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const auth = require('../middleware/auth');
-const messageController = require('../controller/messageController');
-const router = express.Router();
-
-router
-    .post('/message/addmsg', messageController.addMessage)
-    .post('/message/getmsg', messageController.getAllMessage)
-exports.router = router;
--- _Backend/router/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/userController');
-
-router
-    .post('/signup', userController.SignUp)
-    .post('/login', userController.LogIn)
-
-exports.router = router;
